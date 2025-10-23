957214736dd9236ae424850e72f4560f9e3bd5fb
dodo <rivaldoimannuel7@gmail.com>
2025-10-23 14:50:55 +0700

    add: buku.php

---
+++ buku.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Daftar Buku</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SRI14xkILFYv47J16cr9ZwB07vP4J8+Lh7qKQnqukIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container mt-4">
+        <table class="table table-striped">
+            <!-- ini untuk heading table -->
+            <thead>
+                <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Judul</th>
+                    <th scope="col">Penulis</th>
+                    <th scope="col">Penerbit</th>
+                    <th scope="col">Stok</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- isi data nanti ditambahkan di sini -->
+            </tbody>
+        </table>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKycoFOrCGIvvvx9Hj00JCYn3nv7wiPv1ZYyw1rWvCX/BmnDVxM+D2scQbTTxT" crossorigin="anonymous"></script>
+</body>
+</html>
